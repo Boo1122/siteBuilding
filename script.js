53246fcc4c205576ff9d4a5c4b98fce3c32c94c1
Boo1122 <you@example.com>
2020-03-12 22:25:56 +0200

    addEvents

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log('start')
+
+let home = document.getElementById('home');
+home.addEventListener('click', function (e) {
+    console.log('home')
+})
+
+
+let dropdown = document.getElementById('navbarDropdown');
+dropdown.addEventListener('click', function (e) {
+    console.log('dropdown')
+})
+
+let about = document.getElementById('about');
+about.addEventListener('click', function (e) {
+    console.log('about');
+})
+
+let email = document.getElementById('email');
+email.addEventListener('click', function (e) {
+    console.log('email');
+
+})
+
+let details = document.getElementById('details');
+details.addEventListener('click', function (e) {
+    console.log('about');
+})
+
+let contact = document.getElementById('contact');
+contact.addEventListener('click', function (e) {
+    console.log('contact');
+})
